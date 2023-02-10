ec6de21362b1488d672990fa0d0dfc23f02e0d1f
aihtnyc_h <feiver@naver.com>
2023-02-10 23:39:11 +0900

    2월 10일 항해99 주특기 숙련과정 예제2

---
+++ src/main/java/com/example/myselectshop/service/ProductService.java
@@ -0,0 +1,45 @@
+package com.example.myselectshop.service;
+
+import com.example.myselectshop.dto.ProductMypriceRequestDto;
+import com.example.myselectshop.dto.ProductRequestDto;
+import com.example.myselectshop.dto.ProductResponseDto;
+import com.example.myselectshop.entity.Product;
+import com.example.myselectshop.repository.ProductRepository;
+import org.springframework.stereotype.Component;
+
+import java.sql.SQLException;
+import java.util.List;
+
+
+@Component
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService() {
+        this.productRepository = new ProductRepository();
+    }
+
+    public ProductResponseDto createProduct(ProductRequestDto requestDto) throws SQLException {
+        // 요청받은 DTO 로 DB에 저장할 객체 만들기
+        Product product = new Product(requestDto);
+
+        return  productRepository.createProduct(product);
+    }
+
+    public List<ProductResponseDto> getProducts() throws SQLException {
+
+        return productRepository.getProducts();
+    }
+
+    public Long updateProduct(Long id, ProductMypriceRequestDto requestDto) throws SQLException {
+        Product product = productRepository.getProduct(id);
+
+        if(product == null) {
+            throw new NullPointerException("해당 상품은 존재하지 않습니다.");
+        }
+
+        return productRepository.updateProduct(product.getId(), requestDto);
+    }
+
+}
