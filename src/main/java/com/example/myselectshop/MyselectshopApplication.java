ec7dbf9cfda64f4b401adf6f2bb2e77644c529a9
aihtnyc_h <feiver@naver.com>
2023-02-11 22:49:49 +0900

    2월 11일 항해99 주특기 숙련과정 예제2

--- src/main/java/com/example/myselectshop/MyselectshopApplication.java
+++ src/main/java/com/example/myselectshop/MyselectshopApplication.java
@@ -2,7 +2,10 @@ package com.example.myselectshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
+@EnableJpaAuditing
+@EnableScheduling
 @SpringBootApplication
 public class MyselectshopApplication {
 
